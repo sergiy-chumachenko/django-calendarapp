81fe21635968e177b10bbdf3c670d764601bcf92
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-04-01 15:14:31 +0300

    [UPDATE] Create A Calendar Application #5 (Forms)

--- calendarproject/urls.py
+++ calendarproject/urls.py
@@ -21,5 +21,6 @@ from myapp import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('entry/<int:pk>', views.details, name='details'),
+    path('entry/add', views.add, name='add'),
     path('admin/', admin.site.urls),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Entry
+from .forms import EntryForm
 
 # Create your views here.
 
@@ -12,3 +14,21 @@ def index(request):
 def details(request, pk):
     entry = Entry.objects.get(id=pk)
     return render(request, 'myapp/details.html', {'entry': entry})
+
+
+def add(request):
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            date = form.cleaned_data['date']
+            description = form.cleaned_data['description']
+            Entry.objects.create(
+                name=name,
+                date=date,
+                description=description
+            ).save()
+            return HttpResponseRedirect('/')
+    else:
+        form = EntryForm()
+    return render(request, 'myapp/form.html', {'form': form})
